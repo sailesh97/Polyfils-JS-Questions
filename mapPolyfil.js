7a48ba5a367bb80b7b796b5754012e659e0330db
sailesh97 <saileshpatra997@gmail.com>
2022-10-20 12:33:29 +0530

    Map Polyfil

---
+++ mapPolyfil.js
@@ -0,0 +1,28 @@
+// Polyfil for map
+
+// Array.map((num, i, arr) => { })
+
+/**
+ * Definition: The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+ */
+
+Array.prototype.myMap = function(cb){
+    let temp = [];
+    for(let i = 0; i < this.length; i++){
+        temp.push(cb(this[i], i, this));
+    }
+
+    return temp;
+};
+
+const nums = [1, 2, 3, 4];
+
+const multipleThree = nums.map((num, i, arr) => {
+    return num * 3;
+});
+
+const multipleThree2 = nums.myMap((num, i, arr) => {
+    return num * 3;
+});
+
+console.log(multipleThree, multipleThree2);
